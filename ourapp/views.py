ce4c542efa151cf4b21e6f5eeab5fc1795ba3588
bayan41 <bayanab1@ac.sce.ac.il>
2024-03-08 00:44:05 +0200

    sign up for parents

--- ourapp/views.py
+++ ourapp/views.py
@@ -1,4 +1,3 @@
-
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.forms import inlineformset_factory
@@ -84,3 +83,21 @@ def loginpsychologist(request):
             messages.info(request, 'username OR password incorrert')
     context = {}
     return render(request, 'ourapp/log_in_psy.html', context)
+
+
+
+
+def sign_up_parent(request):
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get('username')
+            group = Group.objects.get(name='Parents')
+            user.groups.add(group)
+            messages.success(request, 'Account was created for ' + username)
+            return redirect('loginParent')
+    context = {'form': form}
+
+    return render(request, 'ourapp/sign_up_parent.html', context)
