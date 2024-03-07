183f1cadb8efeb12a0267444c09298976dfa5730
Hiba <einas.mansor@gmail.com>
2024-03-07 11:45:21 +0200

    log in teenager page/sign up teenager page

---
+++ ourapp/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('',views.home,name="dashbord"),
+    path('feedback/', views.feedback),
+    path('link/', views.link),
+    path('loginTeenager/', views.loginTeenager,name='loginTeenager'),
+    path('test/', views.test),
+    path('aaa/', views.aaa),
+    path('singupteenger/', views.singupteenager,name='singupteenger'),
+
+]
