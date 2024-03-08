dd70361adaf0ec85ec741cbe26291f4bac4298ea
Abedasbe <abedab4@ac.sce.ac.il>
2024-03-08 13:55:53 +0200

    log_In_psychologist_test

--- ourapp/test_hackathon.py
+++ ourapp/test_hackathon.py
@@ -2,6 +2,24 @@ from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth.models import User
 from django.contrib.auth.models import Group
+class TestLoginPsychologist(TestCase):
+    def setUp(self):
+        self.url = reverse('loginpsychologist')
+        self.user = User.objects.create_user(username='test_user', password='test_password')
+        Group.objects.create(name='Psychotherapist').user_set.add(self.user)
+
+    def test_login_successful(self):
+        # בדיקה כאשר הפרטים שהוזנו הם נכונים
+        response = self.client.post(self.url, {'username': 'test_user', 'password': 'test_password'})
+        self.assertRedirects(response, reverse('dashbord'))
+
+    def test_login_unsuccessful(self):
+        # בדיקה כאשר שם המשתמש או הסיסמה אינם נכונים
+        response = self.client.post(self.url, {'username': 'incorrect_username', 'password': 'incorrect_password'})
+        self.assertContains(response, 'username OR password incorrect')
+from django.urls import reverse
+from django.contrib.auth.models import User
+from django.contrib.auth.models import Group
 import unittest
 import re
 from django.test import RequestFactory
@@ -54,7 +72,21 @@ class TestLoginTeenager(TestCase):
         response = self.client.post(self.url, {'username': 'incorrect_username', 'password': 'incorrect_password'})
         self.assertContains(response, 'username OR password incorrert')
 
+class TestLoginPsychologist(TestCase):
+    def setUp(self):
+        self.url = reverse('loginpsychologist')
+        self.user = User.objects.create_user(username='test_user', password='test_password')
+        Group.objects.create(name='Psychotherapist').user_set.add(self.user)
 
+    def test_login_successful(self):
+        # בדיקה כאשר הפרטים שהוזנו הם נכונים
+        response = self.client.post(self.url, {'username': 'test_user', 'password': 'test_password'})
+        self.assertRedirects(response, reverse('dashbord'))
+
+    def test_login_unsuccessful(self):
+        # בדיקה כאשר שם המשתמש או הסיסמה אינם נכונים
+        response = self.client.post(self.url, {'username': 'incorrect_username', 'password': 'incorrect_password'})
+        self.assertContains(response, 'username OR password incorrert')
 
 
 
