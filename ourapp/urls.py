f17aacd3fb6e6e37d57ef1d4f6ebb0589efa5da3
bayan41 <bayanab1@ac.sce.ac.il>
2024-04-03 10:10:16 +0300

    l

--- ourapp/urls.py
+++ ourapp/urls.py
@@ -14,5 +14,34 @@ urlpatterns = [
     path('loginpsychologist/', views.loginpsychologist, name='loginpsychologist'),
     path('sign_up_parent/', views.sign_up_parent, name='sign_up_parent'),
 
+
+    path('choicelinktopic_teenageer/',views.choicelinktopic_teenager, name='choicelinktopic_teenageer'),
+    path('behavioral_challenges_ten/', views.behavioral_challenges_ten, name='behavioral_challenges_ten'),
+    path('communication_challenges_ten/', views.communication_challenges_ten, name='communication_challenges_ten'),
+    path('emotional_support_ten/', views.emotional_support_ten, name='emotional_support_ten'),
+
+
+    path('choicelinktopic_parent/', views.choicelinktopic_parent, name='choicelinktopic_parent'),
+    path('behavioral_challenges_par/', views.behavioral_challenges_par, name='behavioral_challenges_par'),
+    path('communication_challenges_par/', views.communication_challenges_par, name='communication_challenges_par'),
+    path('time_management_par/', views.time_management_par, name='time_management_par'),
+
+    path('choicelinktopic_psy_par/', views.choicelinktopic_psy_par, name='choicelinktopic_psy_par'),
+    path('choicelinktopic_psy_ten/', views.choicelinktopic_psy_ten, name='choicelinktopic_psy_ten'),
+
+    path('post_e_s_ten/', views.post_e_s_ten, name='post_e_s_ten'),
+    path('post_c_ch_ten/', views.post_c_ch_ten, name='post_c_ch_ten'),
+    path('post_b_ch_ten/', views.post_b_ch_ten, name='post_b_ch_ten'),
+
+    path('post_t_m_par/', views.post_t_m_par, name='post_t_m_par'),
+    path('post_c_ch_par/', views.post_c_ch_par, name='post_c_ch_par'),
+    path('post_b_ch_par/', views.post_b_ch_par, name='post_b_ch_par'),
+
+    path('view_links_psy_par/', views.view_links_psy_par, name='view_links_psy_par'),
+    path('delete_link_psy_par/<int:link_id>/', views.delete_link_psy_par, name='delete_link_psy_par'),
+    path('view_links_psy_ten/', views.view_links_psy_ten, name='view_links_psy_ten'),
+    path('delete_link_psy_ten/<int:link_id>/', views.delete_link_psy_ten, name='delete_link_psy_ten'),
+
+    path('thank_you_page/', views.thank_you_page, name='thank_you_page')
 ]
 # loginParent
--- ourapp/views.py
+++ ourapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect,get_object_or_404
 from django.http import HttpResponse
 from django.forms import inlineformset_factory
 from django.contrib.auth.forms import UserCreationForm
@@ -9,6 +9,10 @@ from django.contrib.auth.models import Group
 from .models import *
 from itertools import count, repeat,chain
 from .forms import CreateUserForm
+# views.py
+from django.shortcuts import render
+from .models import Lecture
+
 
 
 def home(request):
@@ -101,3 +105,170 @@ def sign_up_parent(request):
             return redirect('loginParent')
     context = {'form': form}
     return render(request, 'ourapp/sign_up_parent.html', context)
+
+
+
+def choicelinktopic_teenager(request):
+    return render(request, 'ourapp/choicelinktopic_teenager.html')
+
+def behavioral_challenges_ten(request):
+    links = Lecture.objects.filter(forWhom="Behavioral Challenges For Teenagers")
+    return render(request, 'ourapp/behavioral_challenges_ten.html', {'behavioral_challenges_ten': links})
+
+
+def communication_challenges_ten(request):
+    links = Lecture.objects.filter(forWhom="Communication Challenges For Teenagers")
+    return render(request, 'ourapp/communication_challenges_ten.html', {'communication_challenges_ten': links})
+
+def emotional_support_ten(request):
+    links = Lecture.objects.filter(forWhom="Emotional Support")
+    return render(request, 'ourapp/emotional_support_ten.html', {'emotional_support_ten': links})
+
+def choicelinktopic_parent(request):
+    return render(request, 'ourapp/choicelinktopic_parent.html')
+
+def behavioral_challenges_par(request):
+    links = Lecture.objects.filter(forWhom="Behavioral Challenges For Parents")
+    return render(request, 'ourapp/behavioral_challenges_par.html', {'behavioral_challenges_par': links})
+
+def communication_challenges_par(request):
+    links = Lecture.objects.filter(forWhom="Communication Challenges For Parents")
+    return render(request, 'ourapp/communication_challenges_par.html', {'communication_challenges_par': links})
+
+
+def time_management_par(request):
+    links = Lecture.objects.filter(forWhom="Time Management")
+    return render(request, 'ourapp/time_management_par.html', {'time_management_par': links})
+
+def choicelinktopic_psy_par(request):
+    return render(request, 'ourapp/choicelinktopic_psy_par.html')
+def choicelinktopic_psy_ten(request):
+    return render(request, 'ourapp/choicelinktopic_psy_ten.html')
+
+
+
+def post_e_s_ten(request):
+    if request.method == 'POST':
+        link = request.POST.get('link')
+        description = request.POST.get('description')
+        if link and description:
+            Lecture.objects.create(link=link, Description=description, forWhom="Emotional Support")
+            # פעולות נוספות אם יש צורך...
+            return redirect('thank_you_page')  # Assuming 'thank_you_page' is the name of your URL pattern for the Thank You page
+        else:
+            return render(request, 'ourapp/post_e_s_ten.html', {'error_message': 'Invalid form data'})
+    else:
+        return render(request, 'ourapp/post_e_s_ten.html')
+
+def post_c_ch_ten(request):
+    if request.method == 'POST':
+        link = request.POST.get('link')
+        description = request.POST.get('description')
+        if link and description:
+            Lecture.objects.create(link=link, Description=description, forWhom="Communication Challenges For Teenagers")
+            # פעולות נוספות אם יש צורך...
+            return redirect('thank_you_page')  # Assuming 'thank_you_page' is the name of your URL pattern for the Thank You page
+        else:
+            return render(request, 'ourapp/communication_challenges_ten.html', {'error_message': 'Invalid form data'})
+
+    else:
+        return render(request, 'ourapp/post_c_ch_ten.html')
+
+
+def post_b_ch_ten(request):
+    if request.method == 'POST':
+        link = request.POST.get('link')
+        description = request.POST.get('description')
+        if link and description:
+            Lecture.objects.create(link=link, Description=description, forWhom="Behavioral Challenges For Teenagers")
+            # פעולות נוספות אם יש צורך...
+            return redirect('thank_you_page')  # Assuming 'thank_you_page' is the name of your URL pattern for the Thank You page
+        else:
+            return render(request, 'ourapp/post_b_ch_ten.html', {'error_message': 'Invalid form data'})
+    else:
+        return render(request, 'ourapp/post_b_ch_ten.html')
+
+
+
+
+def post_t_m_par(request):
+    if request.method == 'POST':
+        link = request.POST.get('link')
+        description = request.POST.get('description')
+        if link and description:
+            Lecture.objects.create(link=link, Description=description, forWhom="Time Management")
+            # Redirect to the Thank You page after successful submission
+            return redirect('thank_you_page')  # Assuming 'thank_you_page' is the name of your URL pattern for the Thank You page
+        else:
+            return render(request, 'ourapp/post_t_m_par.html', {'error_message': 'Invalid form data'})
+    else:
+        return render(request, 'ourapp/post_t_m_par.html')
+
+def post_c_ch_par(request):
+    if request.method == 'POST':
+        link = request.POST.get('link')
+        description = request.POST.get('description')
+        if link and description:
+            Lecture.objects.create(link=link, Description=description, forWhom="Communication Challenges For Parents")
+            # פעולות נוספות אם יש צורך...
+            return redirect('thank_you_page')  # Assuming 'thank_you_page' is the name of your URL pattern for the Thank You page
+        else:
+            return render(request, 'ourapp/post_c_ch_par.html', {'error_message': 'Invalid form data'})
+    else:
+        return render(request, 'ourapp/post_c_ch_par.html')
+
+def post_b_ch_par(request):
+    if request.method == 'POST':
+        link = request.POST.get('link')
+        description = request.POST.get('description')
+        if link and description:
+            Lecture.objects.create(link=link, Description=description, forWhom="Behavioral Challenges For Parents")
+            # Additional actions if needed...
+            return redirect('thank_you_page')  # Assuming 'thank_you_page' is the name of your URL pattern for the Thank You page
+        else:
+            return render(request, 'ourapp/post_b_ch_par.html', {'error_message': 'Invalid form data'})
+    else:
+        return render(request, 'ourapp/post_b_ch_par.html')
+
+
+
+
+
+
+def view_links_psy_par(request):
+    links = Lecture.objects.all()  # Get all links uploaded by the psychologist
+    return render(request, 'ourapp/view_links_psy_par.html', {'links': links})
+
+def delete_link_psy_par(request, link_id):
+    link = get_object_or_404(Lecture, pk=link_id)  # Retrieve the link object or return 404 if not found
+    if request.method == 'POST':
+        # Delete the link
+        link.delete()
+        # Redirect to the psychologist links management page after deletion
+        return redirect('view_links_psy_par')
+    else:
+        # Render a confirmation page if the request method is not POST
+        return render(request, 'delete_link_psy_par.html', {'link': link})
+
+
+def view_links_psy_ten(request):
+    links = Lecture.objects.all()  # Get all links uploaded by the psychologist
+    return render(request, 'ourapp/view_links_psy_ten.html', {'links': links})
+
+def delete_link_psy_ten(request, link_id):
+    link = get_object_or_404(Lecture, pk=link_id)  # Retrieve the link object or return 404 if not found
+    if request.method == 'POST':
+        # Delete the link
+        link.delete()
+        # Redirect to the psychologist links management page after deletion
+        return redirect('view_links_psy_ten')
+    else:
+        # Render a confirmation page if the request method is not POST
+        return render(request, 'delete_link_psy_ten.html', {'link': link})
+
+
+def thank_you_page(request):
+    return render(request, 'ourapp/thank_you_page.html')
+
+
+
